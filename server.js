d218960e441ab91e6678526e554098d6f3170567
Larson04 <lar23016@byui.edu>
2025-09-24 14:17:24 -0600

    intro to routing assignment

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 // Import express using ESM syntax
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT ||3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Create an instance of an Express application
 const app = express();
 
 const name = process.env.NAME; 
 
+
 // Define a route handler for the root URL ('/')
+// app.get('/', (req, res) => {
+//     res.send(`Hello, ${name}!`);
+// });
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
