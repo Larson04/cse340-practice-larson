9297e854beb6d516af25920986b1a6b3f02853ef
Larson04 <lar23016@byui.edu>
2025-10-11 20:22:22 -0600

    Start of Code Challenge 1

---
+++ src/controllers/faculty/faculty.js
@@ -0,0 +1,32 @@
+import { getAllFaculty, getFacultyById, getSortedFaculty } from "../../models/faculty/faculty";
+
+// Route handler for the faculty list page
+export const facultyListPage = (req, res) => {
+    const faculty = getAllFaculty();
+    res.render('faculty-list', { title: 'Faculty List', faculty: faculty });
+    
+}
+
+// Route handler for individual faculty detail pages
+export const facultyDetailPage = (req, res) => {
+    const facultyId = req.params.facultyId;
+    const faculty = getFacultyById(facultyId);
+
+    // If faculty doesn't exist, create 404 error
+    if (!faculty) {
+        const err = new Error(`Faculty ${facultyId} not found`);
+        err.status = 404;
+        return next(err);
+    }
+
+    // Handle sorting if requested
+    const sortBy = req.query.sort || 'time';
+    const sortedFaculty = getSortedFaculty(faculty.name, sortBy);
+
+    res.render('faculty-detail', { 
+        title: `${faculty.name}`, 
+        faculty: { ...faculty, sections: sortedFaculty }, 
+        currentSort: sortBy 
+    });
+}
+
