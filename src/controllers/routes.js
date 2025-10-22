b7e356de7f6d2b02f90cba5e67eda1d4e57aa11f
Larson04 <lar23016@byui.edu>
2025-10-22 15:01:16 -0600

    contact form assignment, testing left

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -3,6 +3,7 @@ import { addDemoHeaders } from '../middleware/demo/headers.js';
 import { courseDetailPage, catalogPage } from './catalog/catalog.js';
 import { homePage,aboutPage, demoPage, testErrorPage } from './index.js';
 import { facultyDetailPage, facultyListPage } from './faculty/faculty.js';
+import { showContactForm, processContactForm, showContactResponses, contactValidation } from './forms/contact.js';
 
 // Create a new router instance
 const router = Router();
@@ -24,5 +25,10 @@ router.get('/test-error', testErrorPage);
 // Faculty routes
 router.get('/faculty', facultyListPage);
 router.get('/faculty/:facultyId', facultyDetailPage);
+
+// Contact form routes
+router.get('/contact', showContactForm);
+router.post('/contact', contactValidation, processContactForm);
+router.get('/contact/responses', showContactResponses);
 
 export default router;
